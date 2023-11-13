3b683f267279d79d8ff096c595ffc2ad9fcd5501
Tim_Lt <timlin@ltedu.com.tw>
2023-11-13 10:00:16 +0800

    chore: upgrade deps

--- layouts/home.vue
+++ layouts/home.vue
@@ -1,9 +1,9 @@
 <template>
   <main class="flex flex-col items-center px-8 py-16 text-center">
     <slot />
-    <Footer />
     <div class="mx-auto mt-5 text-center text-sm opacity-50">
       [ Home Layout ]
     </div>
+    <Footer />
   </main>
 </template>
